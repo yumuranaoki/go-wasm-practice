7e4a6c106c01d362721d84406d31a91d77ddeffb
yumuranaoki <naoki.yumura@gmail.com>
2019-04-29 23:30:03 +0900

    enabled dom manuplation

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"syscall/js"
+)
+
+func increment(this js.Value, values []js.Value) interface{} {
+	document := js.Global().Get("document")
+	root := document.Call("getElementById", "root")
+	root.Set("innerText", "incremented")
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello Wasm")
+	c := make(chan bool)
+	js.Global().Set("increment", js.FuncOf(increment))
+	<-c
 }
